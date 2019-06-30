0c7ea0a406be61aee9beaedf7fd49f12a623dfb5
DaChosens1 <gregoryli1026@gmail.com>
2019-06-30 14:04:55 -0700

    commiting

--- December 2017 Prob 1/2017_December_3_BlockedBillboard.py
+++ December 2017 Prob 1/2017_December_3_BlockedBillboard.py
@@ -1,23 +1,30 @@
-fin = open('billboard.in')
-# fin = open('ex.txt')
-recordings = []
+def leftover_area(input_list, changer list):
+    pass
+# fin = open('billboard.in')
+fin = open('ex.txt')
+billboard1 = []
+billboard2 = []
+truck = []
 count = 1
 for line in fin:
-    if count != 1:
+    if count == 1:
         line = line.strip()
-        days, name, amount = line.split(' ')
-        days = int(days)
-        if "+" in amount:
-            n, amount = amount.split('+')
-        amount = int(amount)
-        recordings.append([days, name, amount])
-    else:
+        x1, y1, x2, y2 = list(map(lambda x: int(x), line.split(' ')))
+        billboard1 = [[x1, y1][x2, y2]]
+    if count == 2:
         line = line.strip()
+        x1, y1, x2, y2 = list(map(lambda x: int(x), line.split(' ')))
+        billboard2 = [[x1, y1][x2, y2]]
+    if count == 3:
+        line = line.strip()
+        x1, y1, x2, y2 = list(map(lambda x: int(x), line.split(' ')))
+        truck = [[x1, y1][x2, y2]]
     count += 1
 fin.close()
-recordings.sort()
+board1 = leftover_area(billboard1, truck)
+board2 = leftover_area(billboard2, truck)
 
-
-fout = open("billboard.out", "w")
-fout.write("{}".format())
-fout.close()
+print(board1 + board2)
+# fout = open("billboard.out", "w")
+# fout.write("{}".format(board1 + board2))
+# fout.close()
