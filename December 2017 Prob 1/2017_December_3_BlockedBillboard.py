f760310121690766955fcbf237ec9801814ec689
DaChosens1 <gregoryli1026@gmail.com>
2019-06-30 14:41:17 -0700

    commiting

--- December 2017 Prob 1/2017_December_3_BlockedBillboard.py
+++ December 2017 Prob 1/2017_December_3_BlockedBillboard.py
@@ -1,7 +1,22 @@
-def leftover_area(input_list, changer list):
-    pass
-# fin = open('billboard.in')
-fin = open('ex.txt')
+def area(x1, y1, x2, y2):
+    return (x2 - x1) * (y2 - y1)
+
+
+def leftover_area(input_list, changer_list):
+    [x1, y1], [x2, y2] = input_list
+    [x3, y3], [x4, y4] = changer_list
+    visible_area = area(x1, y1, x2, y2)
+    leftmostblockedx = max(x1, x3)
+    rightmostblockedx = min(x2, x4)
+    bottommostblockedy = max(y1, y3)
+    topmostblockedy = min(y2, y4)
+    if leftmostblockedx < rightmostblockedx and bottommostblockedy < topmostblockedy:
+        visible_area -= area(leftmostblockedx, bottommostblockedy, rightmostblockedx, topmostblockedy)
+    return visible_area
+
+
+fin = open('billboard.in')
+# fin = open('ex.txt')
 billboard1 = []
 billboard2 = []
 truck = []
@@ -10,21 +25,19 @@ for line in fin:
     if count == 1:
         line = line.strip()
         x1, y1, x2, y2 = list(map(lambda x: int(x), line.split(' ')))
-        billboard1 = [[x1, y1][x2, y2]]
+        billboard1 = [[x1, y1], [x2, y2]]
     if count == 2:
         line = line.strip()
         x1, y1, x2, y2 = list(map(lambda x: int(x), line.split(' ')))
-        billboard2 = [[x1, y1][x2, y2]]
+        billboard2 = [[x1, y1], [x2, y2]]
     if count == 3:
         line = line.strip()
         x1, y1, x2, y2 = list(map(lambda x: int(x), line.split(' ')))
-        truck = [[x1, y1][x2, y2]]
+        truck = [[x1, y1], [x2, y2]]
     count += 1
 fin.close()
 board1 = leftover_area(billboard1, truck)
 board2 = leftover_area(billboard2, truck)
-
-print(board1 + board2)
-# fout = open("billboard.out", "w")
-# fout.write("{}".format(board1 + board2))
-# fout.close()
+fout = open("billboard.out", "w")
+fout.write("{}".format(board1 + board2))
+fout.close()
