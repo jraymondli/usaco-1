55651ebf6393699ff99a6ea215c373344ff842f5
DaChosens1 <gregoryli1026@gmail.com>
2019-07-03 20:52:41 -0700

    Adding 2016 December 3 CowSignal to Git

---
+++ December 2015 Prob 1/2015_December_2_FencePainting.py
@@ -0,0 +1,18 @@
+fin = open('paint.in')
+# fin = open('ex.txt')
+line_points = []
+for line in fin:
+    line = line.strip()
+    one, two = line.split(" ")
+    line_points.append([int(one), int(two)])
+answer = 0
+if line_points[0][1] < line_points[1][0] and line_points[0][0] < line_points[1][1] or \
+        line_points[0][0] > line_points[1][1] and line_points[0][1] > line_points[1][0]:
+    answer = line_points[0][1] - line_points[0][0] + line_points[1][1] - line_points[1][0]
+else:
+    temp = line_points[0] + line_points[1]
+    answer = max(temp) - min(temp)
+fin.close()
+fout = open("paint.out", "w")
+fout.write("{}".format(answer))
+fout.close()
