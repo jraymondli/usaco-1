00b0627d7a2d397227860361154bcc3a43d75e47
DaChosens1 <gregoryli1026@gmail.com>
2019-07-08 22:05:23 -0700

    Adding 2015 December 3 ComtaminatedMilk to Git

--- December 2015 Prob 3/2015_December_3_ContaminatedMilk.py
+++ December 2015 Prob 3/2015_December_3_ContaminatedMilk.py
@@ -1,7 +1,15 @@
+def add_person(input_person, input_dict):
+    if input_person[0] in input_dict.keys():
+        input_dict[input_person[0]].append(input_person[1::])
+    else:
+        input_dict[input_person[0]] = list()
+        input_dict[input_person[0]].append(input_person[1::])
+
+
 fin = open('7.in')
 # fin = open('ex.txt')
-milk_drank = []
-sick = []
+people_drank_milk = []
+sick_people = []
 count = 0
 for line in fin:
     line = line.strip()
@@ -13,38 +21,35 @@ for line in fin:
     else:
         if count > d:
             person, time = list(map(lambda x: int(x), line.split(" ")))
-            sick.append([person, time])
+            sick_people.append([person, time])
         else:
             person, time, milk = list(map(lambda x: int(x), line.split(" ")))
-            milk_drank.append([person, time, milk])
-    count += 1
-milk_drank.sort()
-all_people = {}
-possible_candidates = [True for number in range(m)]
-for people in milk_drank:                           # Makes the dictionary all_people
-    if people[0] in all_people.keys():              #
-        all_people[people[0]].append(people[1::])   #
-    else:                                           #
-        all_people[people[0]] = list()              #
-        all_people[people[0]].append(people[1::])   #
-for sickly in sick:                                     # Iterates through possible_candidates and selects some to turn
-    for milks in all_people[sickly[0]]:                 # into false.
-        if milks[1] >= sickly[1]:                       #
-            possible_candidates[milks[0]-1] = False     #
-max_people = 0
-count = 1
-for milk in possible_candidates:
-    temp = 0
-    if milk:
-        for person in list(all_people.values()):
-            for milks in person:
-                if count == milks[0]:
-                    temp += 1
-                    break
-        if temp > max_people:
-            max_people = temp
+            people_drank_milk.append([person, time, milk])
     count += 1
 fin.close()
+people_drank_milk.sort()
+all_people = {}
+possible_milk_candidates = [True for number in range(m)]
+for persons_milk_drank in people_drank_milk:
+    add_person(persons_milk_drank, all_people)
+for person in sick_people:
+    for milk_drank in all_people[person[0]]:
+        if milk_drank[1] >= person[1]:
+            possible_milk_candidates[milk_drank[0]-1] = False
+max_sick_people = 0
+count = 1
+for milk_candidate in possible_milk_candidates:
+    temp = 0
+    if milk_candidate:
+        for person in list(all_people.values()):
+            for milk_drank in person:
+                if count == milk_drank[0]:
+                    temp += 1
+                    break
+        if temp > max_sick_people:
+            max_sick_people = temp
+    count += 1
+
 fout = open("badmilk.out", "w")
-fout.write("{}".format(max_people))
+fout.write("{}".format(max_sick_people))
 fout.close()
