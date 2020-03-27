3eff5f91db857f9ab313beb06a8ab4b4f81a78dc
DaChosens1 <48633888+DaChosens1@users.noreply.github.com>
2020-03-26 18:24:21 -0700

    adding usaco problem files

---
+++ 2017/March/February 2017 Prob 1/2017_March_1_lostcow.py
@@ -0,0 +1,36 @@
+# time = 48 min
+def open_file():
+    fin = open('lostcow.in')
+    for line in fin:
+        line = line.strip()
+        places = map(lambda x: int(x), line.split(" "))
+    fin.close()
+    return places
+
+
+def follow_through(places):
+    person = places[0]
+    cow = places[1]
+    change_factor = 1
+    dist = 0
+    while True:
+        moved = 0
+        for move in range(abs(places[0]-person)+abs(change_factor)):
+            if change_factor >= 0:
+                person += 1
+            else:
+                person -= 1
+            moved += 1
+            dist += 1
+            if person == cow:
+                return dist
+        change_factor *= -2
+
+
+def close_file(answer):
+    fout = open("lostcow.out", "w")
+    fout.write("{}".format(answer))
+    fout.close()
+
+
+close_file(follow_through(open_file()))
